d10240bc7401fc730fd78c47891126dad0efab57
Daniel <dwgrossberg@gmail.com>
2022-08-26 07:56:15 -0400

    Add buildTree function

--- Tree.js
+++ Tree.js
@@ -1,17 +1,40 @@
 import mergeSort from "../binary-search-tree/mergeSort";
+import Node from "./Node";
 
-const Tree = (array) => {
-  const sortArray = (arr) => {
-    const arrWithoutDuplicates = [...new Set(arr)];
-    const sortedArr = mergeSort(arrWithoutDuplicates);
-    return sortedArr;
+const Tree = (inputArray) => {
+  const buildTree = (arr, start, end) => {
+    // base case
+    if (end > start) return;
+    // find the middle
+    const mid = Math.floor((start + end) / 2);
+    const root = Node(arr[mid]);
+    // recursively construct left subtree
+    root.leftChild = buildTree(arr, start, mid - 1);
+    // recursively construct right subtree
+    root.rightChild = buildTree(arr, mid + 1, end);
+    return root;
   };
 
-  const buildTree = (arr) => {};
+  const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
 
-  const root = buildTree(array);
+  //   prettyPrint(root);
+
+  const array = [...new Set(mergeSort(inputArray))];
+  const root = buildTree(array, 0, array.length - 1);
+  const preOrderData = [];
+  const inOrderData = [];
+  const postOrderData = [];
 
   return {
+    array,
     root,
     sortArray,
     buildTree,
--- Tree.test.js
+++ Tree.test.js
@@ -2,18 +2,17 @@
 import Tree from "../binary-search-tree/Tree";
 import Node from "../binary-search-tree/Node";
 
-const tree = Tree();
+const tree1 = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const tree2 = Tree([0.1, 0.7, -4, 23, 800, 0.9, 40, -3, 500, -7]);
 
 describe("Tree factory methods", () => {
   test("happy path", () => {
     expect(Tree);
   });
   test("buildTree() can remove duplicate values and sort array into order", () => {
-    expect(
-      tree.sortArray([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-    ).toStrictEqual([1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]);
-    expect(
-      tree.sortArray([0.1, 0.7, -4, 23, 800, 0.9, 40, -3, 500, -7])
-    ).toStrictEqual([-7, -4, -3, 0.1, 0.7, 0.9, 23, 40, 500, 800]);
+    expect(tree1.array).toStrictEqual([1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]);
+    expect(tree2.array).toStrictEqual([
+      -7, -4, -3, 0.1, 0.7, 0.9, 23, 40, 500, 800,
+    ]);
   });
 });
